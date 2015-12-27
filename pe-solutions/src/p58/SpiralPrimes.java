82a6cd5eea3caf1043b767d71ad9304c2005d64a
s41m1r <saimir.bala@gmail.com>
2015-12-27 09:18:40 +0100

    Implementing solution for p54

--- pe-solutions/src/p58/SpiralPrimes.java
+++ pe-solutions/src/p58/SpiralPrimes.java
@@ -16,15 +16,12 @@ public class SpiralPrimes {
 	 */
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
-		Spiral spiral = new Spiral();
-		spiral.wrapOneLayer();
-		spiral.recomputeDiagonals();
 		double ratioPrimesOverTotal = 1;
 		int primes = 0;
 		int sideLength = 1;
 		
 		for(sideLength=1; ratioPrimesOverTotal>.1; sideLength+=2){
-			int spiralLength = (int) Math.pow(sideLength+2, 2);
+			int spiralLength = (sideLength+2)*(sideLength+2);
 			for(int i = sideLength*sideLength+1; i<=spiralLength; i++){
 				if(isDiagonalMember(sideLength+2, i)){
 					if(CommonFunctions.isPrime(i)){
@@ -34,10 +31,10 @@ public class SpiralPrimes {
 			}
 			ratioPrimesOverTotal = primes/(double)totalNumbersInDiagonals(sideLength+2);
 		}
+		long end = System.currentTimeMillis();
 		System.out.println("side length = "+sideLength);
 		System.out.println("primes="+primes);
 		System.out.println("total nums = "+totalNumbersInDiagonals(sideLength));
-		long end = System.currentTimeMillis();
 		System.out.println("Percentage is "+ratioPrimesOverTotal);
 		System.out.println("It took "+(end-start)+" millisecondss.");
 	}
