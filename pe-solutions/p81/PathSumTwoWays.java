00b5fd5c9ade0945df7e4690ce53a43110988128
s41m1r <saimir.bala@gmail.com>
2017-07-11 18:03:52 -0300

    Create class for p81

---
+++ pe-solutions/p81/PathSumTwoWays.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package p81;
+
+/**
+ * @author Saimir Bala
+ *
+ */
+public class PathSumTwoWays {
+
+	/**
+	 * 
+	 */
+	public PathSumTwoWays() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
